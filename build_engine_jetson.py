2534e853e017546e8ae6818dbf56d9478a4da31a
godhj <godhj@unist.ac.kr>
2024-09-05 10:50:42 +0000

    Add: build_engine_jetson.py
    Former-commit-id: 50894dc728b189d24169d50bfa305170b5fe79cc

---
+++ build_engine_jetson.py
@@ -0,0 +1,28 @@
+import torch_tensorrt
+import torch
+from termcolor import colored
+
+def main():
+    
+    weights = 'runs/train/yolov9-c/weights/yolov9-c-converted_ptq.jit'
+    model = torch.jit.load(weights)
+    
+    B, C, H, W = (1, 3, 382, 674)
+    
+    trt_mod = torch_tensorrt.compile(
+            model, 
+            inputs=[torch.randn((1, C, H, W)).to('cuda')],
+            enabled_precisions={torch.int8},  # Use int8 precision
+            truncate_long_and_double=True,
+        )
+    
+    # Get GPU name
+    gpu_name = torch.cuda.get_device_name(0)
+    weights_path = weights.replace('.pt', f'_qat_{gpu_name}.torchscript').replace(' ', '_')
+    torch.jit.save(trt_mod, weights_path)
+    print(colored(f"The model saved to {weights_path}", 'green'))
+
+
+if __name__ == "__main__":
+    
+    main()
